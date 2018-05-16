6485c46b4420f19bd9d6393574603e0ec7af5693
sentient64 <m.alsharekh94@gmail.com>
2018-05-16 15:50:26 +0300

    formatted code + added .gitignore

--- shop.py
+++ shop.py
@@ -1,10 +1,10 @@
 ####################### DO NOT MODIFY THIS CODE ########################
 menu = {
-	"original cupcake": 2,
-	"signature cupcake": 2.750,
-	"coffee": 1,
-	"tea": 0.900,
-	"bottled water": 0.750
+    "original cupcake": 2,
+    "signature cupcake": 2.750,
+    "coffee": 1,
+    "tea": 0.900,
+    "bottled water": 0.750
 }
 original_flavors = ["vanilla", "chocolate", "strawberry", "caramel", "raspberry"]
 original_price = 2
@@ -15,85 +15,68 @@ cupcake_shop_name = #complete me!
 signature_flavors = #complete me!
 order_list = []
 
+
 def print_menu():
-	"""
-	Print the items in the menu dictionary.
-	"""
+    """
+    Print the items in the menu dictionary.
+    """
+    # your code goes here!
 
-	# your code goes here!
 
-
-#------------------ START -------------------#
 def print_originals():
-	"""
-	Print the original flavor cupcakes.
-	"""
-	print("Our original flavor cupcakes (KD %s each):" % original_price)
-
-	# your code goes here!
-
-#------------------ FINISH -------------------#
+    """
+    Print the original flavor cupcakes.
+    """
+    print("Our original flavor cupcakes (KD %s each):" % original_price)
+    # your code goes here!
 
 
-#------------------ START -------------------#
 def print_signatures():
-	"""
-	Print the signature flavor cupcakes.
-	"""
-	print("Our signature flavor cupcake (KD %s each):" % signature_price)
+    """
+    Print the signature flavor cupcakes.
+    """
+    print("Our signature flavor cupcake (KD %s each):" % signature_price)
+    # your code goes here!
 
-	# your code goes here!
 
-#------------------ FINISH -------------------#
-
-#------------------ START -------------------#
 def is_valid_order(order):
-	"""
-	Check if an order exists in the shop.
-	"""
+    """
+    Check if an order exists in the shop.
+    """
+    # your code goes here!
 
-	# your code goes here!
 
-#------------------ FINISH -------------------#
-
-#------------------ START -------------------#
-# user input loop for order_list
 def get_order():
-	"""
-	Repeatedly ask customer for order until they end their order by typing "Exit".
-	"""
-	order_list = []
+    """
+    Repeatedly ask customer for order until they end their order by typing "Exit".
+    """
+    order_list = []
+    # your code goes here!
 
-	# your code goes here!
-
-	return order_list
-#------------------ FINISH -------------------#
+    return order_list
 
 
 def accept_credit_card(total):
-	"""
-	Return whether an order is eligible for credit card payment.
-	"""
-	# your code goes here!
+    """
+    Return whether an order is eligible for credit card payment.
+    """
+    # your code goes here!
+
 
 def get_total_price(order_list):
-	"""
-	Calculate and return total price of the order.
-	"""
-	total = 0
+    """
+    Calculate and return total price of the order.
+    """
+    total = 0
+    # your code goes here!
 
-	# your code goes here!
+    return total
 
-	return total
 
-#------------------ START -------------------#
 def print_order(order_list):
-	"""
-	Print the order of the customer.
-	"""
-	print()
-	print("Your order is: ")
-
-	# your code goes here!
-
-#------------------ FINISH -------------------#
+    """
+    Print the order of the customer.
+    """
+    print()
+    print("Your order is: ")
+    # your code goes here!
